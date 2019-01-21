14232bf8db0e5315d86468294cea224016fa5d28
Cobeasta <stopyloknok@gmail.com>
2019-01-21 17:36:37 -0600

    Code for a ball intake

--- MyfirstRobot/src/main/java/frc/robot/Robot.java
+++ MyfirstRobot/src/main/java/frc/robot/Robot.java
@@ -12,8 +12,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.BallIntake;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,9 +22,9 @@ import frc.robot.subsystems.ExampleSubsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI m_oi;
   Command m_autonomousCommand;
+  public static BallIntake ballIntake;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   /**
@@ -35,8 +34,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    
+    ballIntake = new BallIntake();    
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
   
