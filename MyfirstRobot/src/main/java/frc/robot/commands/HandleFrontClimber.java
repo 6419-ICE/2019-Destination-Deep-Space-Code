fb0de323ef120d159ac2ce3f4289a98807b5a973
Cobeasta <stopyloknok@gmail.com>
2019-02-11 20:52:33 -0600

    handle front climber quick fix

--- MyfirstRobot/src/main/java/frc/robot/commands/HandleFrontClimber.java
+++ MyfirstRobot/src/main/java/frc/robot/commands/HandleFrontClimber.java
@@ -12,32 +12,38 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class HandleFrontClimber extends Command {
-  public HandleFrontClimber() {
+  public HandleFrontClimber()
+  {
+    requires(Robot.frontClimber);
+    
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
 
   // Called just before this Command runs the first time
   @Override
-  protected void initialize() {
+  protected void initialize()
+  {
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  protected void execute() {
+  protected void execute()
+  {
     Robot.frontClimber.holdPosition();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
-  protected boolean isFinished() {
+  protected boolean isFinished()
+  {
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    Robot.frontClimber.
+    Robot.frontClimber.set(0);
   }
 
   // Called when another command which requires one or more of the same
