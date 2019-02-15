7db49d6190e338d84e63be7490766b14f65d02f3
Cobeasta <stopyloknok@gmail.com>
2019-02-14 20:15:50 -0600

    raise climber commands only run once

--- MyfirstRobot/src/main/java/frc/robot/commands/RaiseBackClimber.java
+++ MyfirstRobot/src/main/java/frc/robot/commands/RaiseBackClimber.java
@@ -34,7 +34,7 @@ public class RaiseBackClimber extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return Robot.backClimber.getRaised();
+    return true;
   }
 
   // Called once after isFinished returns true
