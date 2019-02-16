2c77205a236309ee4581cc17e2de41c890aed90c
Cobeasta <stopyloknok@gmail.com>
2019-02-16 11:29:48 -0600

    made wrist position file

--- MyfirstRobot/src/main/java/frc/robot/subsystems/Wrist.java
+++ MyfirstRobot/src/main/java/frc/robot/subsystems/Wrist.java
@@ -5,20 +5,39 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
 
 /**
  * Add your docs here.
  */
 public class Wrist extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+  private VictorSPX motor;
+  public static int DOWN = 0;
+  public static int MIDDLE = 1000;
+  public static int up = 2000;
 
+  public Wrist()
+  {
+    motor = new VictorSPX(RobotMap.WRIST);
+  }
+
+  public void set(double power)
+  {
+    motor.set(ControlMode.PercentOutput, power);
+  }
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+  public void setPosition()
+  {
+    
   }
 }
