00203514c4fb977383cebd0723f463a17fbe8684
Cobeasta <stopyloknok@gmail.com>
2019-03-09 16:57:48 -0600

    Raiseing commands

---
+++ MyfirstRobot/src/main/java/frc/robot/commands/RaiseFront.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.ClimberDirection;
+import frc.robot.Robot;
+
+public class RaiseFront extends Command {
+  private ControlMode mode;
+  private double power;
+
+  public RaiseFront(ControlMode mode, double power) {
+    requires(Robot.climber);
+    this.mode = mode;
+  this.power = power;
+
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.climber.disable();
+    Robot.climber.setFront(mode, power);
+
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
